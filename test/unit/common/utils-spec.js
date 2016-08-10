f77c05fa9e44a88e4f6b2f93220ff1ff9b3dbc5d
Sukrit Khera <sukrit007@gmail.com>
2016-08-10 11:04:53 -0700

    Initial library structure

---
+++ test/unit/common/utils-spec.js
@@ -0,0 +1,40 @@
+"use strict";
+
+require('../../init-chai');
+
+const
+  commonUtils = require('../../../lib/common/utils'),
+  sinon = require('sinon');
+
+describe('utils', () => {
+
+  describe('nextRetryInterval', () => {
+
+    it('should calculate next retry interval', () => {
+      commonUtils.nextRetryInterval(3).should.be.equal(68);
+    });
+
+    it('should limit max interval', () => {
+      commonUtils.nextRetryInterval(10).should.be.equal(600);
+    });
+
+  });
+
+  describe('wait', () => {
+    let clock;
+
+    before(() => {
+      clock = sinon.useFakeTimers();
+    });
+
+    after(() => {
+      clock.restore();
+    });
+
+    it('should wait for given duration', () => {
+      let waitPromise =  commonUtils.wait(1000);
+      clock.tick(1000);
+      return waitPromise.should.be.fulfilled;
+    });
+  });
+});
