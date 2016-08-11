b4585d12ad4fb6f8a3b920e2694bca66c4d13c93
Sukrit Khera <sukrit007@gmail.com>
2016-08-10 17:53:15 -0700

    Rename package to aws-sdk-ext

--- examples/sqs/sqs-consumer-example.js
+++ examples/sqs/sqs-consumer-example.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const SqsConsumer =   require('aws-sdk-js-ext').sqs.SqsConsumer, //require('../../lib').sqs.SqsConsumer
+const SqsConsumer =   require('aws-sdk-ext').sqs.SqsConsumer, //require('../../lib').sqs.SqsConsumer
   winston = require('winston'),
   promisify = require('es6-promisify'),
   config = require('config'),
